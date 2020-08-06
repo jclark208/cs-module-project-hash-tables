f1c8c142e75c1a67e4fc18783f63c3fcaca71df1
Jake Clark <jclark208@yahoo.com>
2020-08-06 09:42:33 -0400

    day3 stuff

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,6 +1,39 @@
 def word_count(s):
     # Your code here
+    s = s.lower()
+    ignore = [
+        '"',
+        ":",
+        ";",
+        ",",
+        ".",
+        "-",
+        "+",
+        "=",
+        "/",
+        "\\",
+        "|",
+        "[",
+        "]",
+        "{",
+        "}",
+        "(",
+        ")",
+        "*",
+        "^",
+        "&",
+    ]
 
+    for char in ignore:
+        s = s.replace(char, "")
+    counts = {}
+    split = s.split()
+    for word in split:
+        if word in counts.keys():
+            counts[word] += 1
+        else:
+            counts[word] = 1
+    return counts
 
 
 if __name__ == "__main__":
